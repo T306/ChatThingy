092541acef22d6713afd8df07a0dcd9870568fa0
T306 <57376135+T306@users.noreply.github.com>
2023-03-30 13:24:07 +0000

    A lot

--- main.py
+++ main.py
@@ -3,42 +3,58 @@ import sys
 import argon2
 import socket
 import quantumrandom as  qrand
-from nacl.public import PrivateKey, Box
-from nacl.encoding import URLSafeBase64Encoder
-from nacl.signing import SigningKey
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
-signing_key=SigningKey.generate()
-verify_key=signing_key.verify_key
-verify_key_b64=verify_key.encode(encoder=URLSafeBase64Encoder)
+privKey = ec.generate_private_key(
+    ec.SECP384R1()
+)
+pubKey = privKey.public_key()
+partKey = None
+shared_key = None
+derived_key = None
 
 
 def send_message(c):
   while True:
-    message=input('')
-    c.send((user+message).encode())
-    print(user+message)
+    message = input('')
+    c.send((user + ': ' + message).encode())
+    print(user + ': ' + message)
 def recv_message(c):
   while True:
-    message=input('')
-    c.send(message.encode())
-    print(c.recv(1024).decode())
+    plain=c.recv(1024).decode()
+    print(plain)
     
 
-user=input('Username: ')
+user=input('Enter username: ')
 choice=input('Host or Connect?\n')
 
 if choice.lower() == 'host':
-  server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-  server.bind(('172.31.128.50',443))
+  server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+  #hostname = input('Enter Hostname or IP')
+  server.bind(('0.0.0.0',443))
   server.listen()
 
-  client,_=server.accept()
-elif choice.lower()=='client':
-  serveraddr=input('Server IP: ')
-  client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+  client,_ = server.accept()
+  client.send(pubKey)
+  partKey = client.recv(1024)
+elif choice.lower() == 'client':
+  serveraddr = input('Server IP: ')
+  client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
   client.connect((serveraddr,443))
+  client.send(pubKey)
+  partKey = client.recv(1024)
 else:
   sys.exit('Invalid Option')
+
+shared_key = private_key.exchange(ec.ECDH(), partKey)
+derived_key = HKDF(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=None,
+    info=b'handshake data',
+).derive(shared_key)
 
 threading.Thread(target=send_message,args=(client,))
 threading.Thread(target=recv_message,args=(client,))
